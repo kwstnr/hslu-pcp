8ad2356cd7d51783fe9876938640c5cd0e4ebaa3
Kevin Wüstner <kevin.wuestner@xappido.com>
2025-02-21 17:21:56 +0100

    feat: implement ex-1b

--- sw01/ex-1/main.c
+++ sw01/ex-1/main.c
@@ -9,7 +9,15 @@ void printNumbersGoto(int number) {
   if (i <= number) goto start;
 }
 
+void printNumbersFor(int number) {
+  for (int i = 0; i <= number; i++) {
+    printf("%i ", i);
+  }
+}
+
 int main(int argc, char** argv) {
   printNumbersGoto(7);
   printf("= printNumbersGoto(7)\n");
+  printNumbersFor(7);
+  printf("= printNumbersFor(7)\n");
 } 
