f1532b5e475a14ac525147b70f1a11517ff7938b
Kevin Wüstner <kevin.wuestner@xappido.com>
2025-02-28 15:52:49 +0100

    feat(sw02): add ex-1a

---
+++ sw02/ex-1/relations.pl
@@ -0,0 +1,34 @@
+% all females
+female(mary). female(liz). female(mia). female(tina). female(ann). female(sue).
+
+% all males
+male(mike). male(jack). male(fred). male(tom). male(joe). male(jim).
+
+% all childern of mary
+parent(mary, mia). parent(mary, fred). parent(mary, tina).
+
+% all children of mike
+parent(mike, mia). parent(mike, fred). parent(mike, tina).
+
+% allchildern of liz
+parent(liz, tom). parent(liz, joe).
+
+% all childern of jack
+parent(jack, tom). parent(jack, joe).
+
+% all childern of mia
+parent(mia, ann).
+
+% all childern of tina
+parent(tina, sue). parent(tina, jim).
+
+% all childern of tom
+parent(tom, sue). parent(tom, jim).
+
+mother(X, Y) :-
+  female(X),
+  parent(X, Y).
+
+father(X, Y) :-
+  male(X),
+  parent(X, Y).
