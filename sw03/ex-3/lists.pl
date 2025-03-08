1cc828c49e0ddc9995933f496c0d42a4ccb2226a
Kevin Wüstner <kevin.wuestner@xappido.com>
2025-03-08 08:46:38 +0100

    docs(sw03): cleanup comments for ex-3a

--- sw03/ex-3/lists.pl
+++ sw03/ex-3/lists.pl
@@ -1,5 +1,5 @@
 % 3a)
-% Die Liste wird rekursiv die Liste [H | T] leeren,
+% Die Liste [H | T] wird rekursiv leeren,
 % der Fakt f1 true ist, und dann die Liste wieder "füllen"
 % bis die komplette Liste wieder "vollständig" ist.
 add_tail(X, [], [X]). % f1
