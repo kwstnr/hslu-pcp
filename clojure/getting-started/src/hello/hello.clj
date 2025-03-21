4826deed9ccd12c93b1df47dc99722b04c86ab97
Kevin Wüstner <kevin.wuestner@xappido.com>
2025-03-21 15:42:51 +0100

    feat(clojure): add clojure getting-started playground

--- clojure/getting-started/src/hello/hello.clj
+++ clojure/getting-started/src/hello/hello.clj
@@ -1,3 +1,29 @@
 (ns hello.hello)
 
 (+ 1 1)
+
+(+ 1 (* 2 3))
+
+(* 5 3)
+
+(type (* 21 2.0))
+
+(/ 16 2 2 2 2)
+
+(< 1 2 3 4 4)
+
+(<= 1 2 3 4 4)
+
+(and 1 42 17.0 true "hello")
+
+(and "test" nil 0)
+
+(and 0 1)
+
+(str "Hello" "World")
+
+(str 1 2)
+
+(def x '(+ 2 3 4 5))
+
+(eval x)
