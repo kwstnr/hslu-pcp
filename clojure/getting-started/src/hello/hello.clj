345b8e9e34d5d29e805a2e78f15f9daca9f89ce5
Kevin Wüstner <kevin.wuestner@xappido.com>
2025-04-03 20:18:05 +0200

    feat(getting-started): add developed playground

--- clojure/getting-started/src/hello/hello.clj
+++ clojure/getting-started/src/hello/hello.clj
@@ -43,3 +43,67 @@
 (profit 5)
 
 (rational? 1/2)
+
+(defn length [lst]
+  (if (empty? lst)
+    0
+    (inc (recur (rest lst)))))
+
+(defn length-2 [lst acc]
+  (if (empty? lst)
+    acc
+    (recur (rest lst) (inc acc))))
+
+(defn length-4 [lst]
+  (loop [local-list lst
+         acc 0]
+    (if (empty? local-list)
+      acc
+      (recur (rest local-list) (inc acc)))))
+
+(length 10000)
+
+(defn double [start
+              length]
+  (loop [i 0
+         acc (cons start nil)]
+    (if (== i length)
+      acc
+      (recur (inc i) (concat acc (cons (* 2 (first acc)) nil))))))
+
+(double 1 8)
+
+(fn? (fn [x] x))
+
+(* 1 2 2 2)
+
+(- 1 1)
+
+(some #(= % 5) [1 2 3 4 5])
+
+(defn hashtag-plus [a b] (+ a b))
+
+(reduce #(+ %1 %2) [1 2 3])
+(map #(+ %1 %2) [[1 1] [2 2] [3 3]])
+
+(map (fn [x] (apply #(+ %1 %2) x)) [[1 1] [2 2] [3 3]])
+(map #(apply #(+ %1 %2) %) [[1 1] [2 2] [3 3]])
+(map #(+ %1 %2) [1 2 3] [1 2 3])
+
+(take-while even? [2 4 6 7 8 10])
+(drop-while even? [2 4 6 7 8 10])
+(some even? [2 4 6 7 8 10])
+
+
+(defn sum-numbers [nums]
+  (map println nums) ; println wird nie ausgeführt
+  (reduce + nums))
+
+(defn sum-numbers-with-print [nums]
+  (reduce (fn [acc, curr]
+            (do
+              (println curr)
+              (+ acc curr))) nums))
+
+(sum-numbers (range 10))
+(sum-numbers-with-print (range 10))
